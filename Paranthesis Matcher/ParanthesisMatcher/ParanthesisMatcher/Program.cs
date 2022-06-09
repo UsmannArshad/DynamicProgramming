c5590d55d7a0ada7b51f6aabd0186ad3246b35d6
UsmannArshad <usmanarshadbhatti480@gmail.com>
2022-06-09 17:49:27 +0500

    Paranthesis matcher

---
+++ Paranthesis Matcher/ParanthesisMatcher/ParanthesisMatcher/Program.cs
@@ -0,0 +1,30 @@
+﻿string a = Console.ReadLine();
+Stack<char> stack = new Stack<char>();
+bool check = true;
+foreach(char b in a)
+{
+    if(b=='{')
+    {
+        stack.Push(b);  
+    }
+    else if(b=='}')
+    {
+        if (stack.Count > 0)
+        {
+            stack.Pop();
+        }
+        else
+        {
+            check = false;
+            break;
+        }
+    }
+}
+if(stack.Count>0||check==false)
+{
+    Console.WriteLine("Invalid");
+}
+else
+{
+    Console.WriteLine("Valid");
+}
