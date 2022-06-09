6a3d56af5be3071021372e639d268891008f94ca
UsmannArshad <usmanarshadbhatti480@gmail.com>
2022-06-09 16:51:51 +0500

    Find nth ugly number

---
+++ nth Ugly Number/nth_Uglyno/nth_Uglyno/Program.cs
@@ -0,0 +1,33 @@
+﻿int a=Convert.ToInt32(Console.ReadLine());
+int nth_ugly = nthUglyno(a);
+Console.WriteLine(nth_ugly);
+int MaxDivide(int a,int b)
+{
+    while(a%b==0)
+    {
+        a = a / b;
+    }
+    return a;
+}
+bool IsUgly(int a)
+{
+    a = MaxDivide(a, 2);
+    a = MaxDivide(a, 3);
+    a = MaxDivide(a, 5);
+    return a== 1;
+}
+int nthUglyno(int a)
+{
+    int number = 0;
+    int uglyCount = 0;
+    while(uglyCount<a)
+    {
+        number++;
+        bool check=IsUgly(number);
+        if(check==true)
+        {
+            uglyCount++;
+        }
+    }
+    return number;
+}
