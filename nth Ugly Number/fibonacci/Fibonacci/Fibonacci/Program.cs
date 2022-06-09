02ceaab99dfaed7c8a47a9eafd1944adfbb3bd48
UsmannArshad <usmanarshadbhatti480@gmail.com>
2022-06-09 17:06:48 +0500

    fibonacci

---
+++ nth Ugly Number/fibonacci/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,14 @@
+﻿int a=Convert.ToInt32(Console.ReadLine());
+int fib = nthfibonacci(a);
+Console.WriteLine(fib);
+int nthfibonacci(int a)
+{
+    if(a<=1)
+    {
+        return a;
+    }
+    else
+    {
+        return nthfibonacci(a - 1) + nthfibonacci(a - 2);
+    }
+}
