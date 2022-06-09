f25349d11d51a2346e47f8582fa1562d38a1fb2a
UsmannArshad <usmanarshadbhatti480@gmail.com>
2022-06-09 17:25:18 +0500

    fibonacci 2nd method

--- nth Ugly Number/fibonacci/Fibonacci/Fibonacci/Program.cs
+++ nth Ugly Number/fibonacci/Fibonacci/Fibonacci/Program.cs
@@ -1,7 +1,7 @@
 ﻿int a=Convert.ToInt32(Console.ReadLine());
 int fib = nthfibonacci(a);
 Console.WriteLine(fib);
-int nthfibonacci(int a)
+/*int nthfibonacci(int a)
 {
     if(a<=1)
     {
@@ -11,4 +11,18 @@ int nthfibonacci(int a)
     {
         return nthfibonacci(a - 1) + nthfibonacci(a - 2);
     }
+}*/
+int nthfibonacci(int a)
+{
+    int first = 0;
+    int second = 1;
+    int c = 0;
+    for(int i=2;i<=a;i++)
+    {
+        c= first + second;
+        first = second;
+        second = c;
+    }
+    return c;
+
 }
